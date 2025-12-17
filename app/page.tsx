023a0a052284605b45bafbdea867e63c1ac35016
Yilin <yilin.zh135@gmail.com>
2025-12-17 16:25:48 +0100

    Add Skills component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Skills from "@/components/Skills";
+
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-slate-950">
       <Hero />
       <About />
+      <Skills />
     </main>
   );
 }
